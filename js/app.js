989a2ff6feed27141649bf0347abbf23102e9b6a
Adam Horton <adam.horton@adamhortons-MacBook-Pro-4465.local>
2020-07-14 00:03:22 -0500

    update from hostgatorbackup

--- js/app.js
+++ js/app.js
@@ -14,10 +14,22 @@ define([
 
 
 	$(window).on('resize', Shared.resize);
+	$(window).on('deviceorientation', function (e) {
+		setTimeout(function () {
+			Shared.resize();
+		}, 300);
+	});
 
 	$('button.bg-minimize').on('click', Shared.minimize);
 	$('button.bg-maximize').on('click', Shared.maximize);
-	setTimeout(Shared.resize, 500);
+	setTimeout(function() {
+		Shared.resize();
+		Shared.setSlide(window.location.hash.replace(/#/,''), true);
+
+//		$(window).on('hashchange', function (e) {
+//		});
+//		$(window).trigger('hashchange');
+	}, 500);
 
   
 	$('main').on('scroll', function (e) {
@@ -28,13 +40,32 @@ define([
 
 		$sections.each(function (i, v) {
 			currPos = parseInt($(v).position().top, 10);
-			posDiff = currPos - scrollTop;
-			//console.log(currPos, scrollTop, posDiff);
-			if (posDiff <= 70) {
-				//console.log('hit: ' + i);
+			if (Math.abs(currPos) <= 180) {
+				Shared.setSlide(i+1);
 				return false; // reportedly breaks out of each
 			}
 		});
+	});
+
+	$(window).on('keydown', function (e) {
+		var code = e.which || e.keyCode || e.key;
+
+		// support vim gGj & k
+		if (code === 40 || code === 74) { // down or 'j'
+			Shared.slideDown();
+		}
+		if (code === 38 || code === 75) { // up or 'k'
+			Shared.slideUp();
+		}
+		if (code === 71) { // g
+			Shared.goTop();
+		}
+		if (code === 71 && e.shiftKey) { // G
+			Shared.goBottom();
+		}
+		if (code === 13) { // enter
+			Shared.lockOn();
+		}
 	});
 
 	Shared.applyFrameTriggers();
--- js/shared.js
+++ js/shared.js
@@ -2,8 +2,9 @@
 //TODO: write dot template plugin
 define([
 	'dot',
-	'jquery'
-], function (dot, $) {
+	'jquery',
+	'clippy'
+], function (dot, $, clippy) {
 	"use strict";
 
 	var ret = {}
@@ -12,10 +13,15 @@ define([
 	,   $footer = $('footer')
 	,   $primaryContainer = $('.container.primary')
 	,   $contentContainer = $('main').find('.container')
+	,   $sections = $('.slideContainer section')
 	,   $gameframe = $('iframe.game')
+	,   agent = null
 	;
 
 	ret.dot = dot;
+	ret.clippy = clippy;
+	ret.slide = 1;
+	ret.lastSlide = 0;
 
 	ret.swapVis  = function ($replaceWhat, $replaceWith) {
 		$($replaceWhat).hide();
@@ -27,8 +33,12 @@ define([
 		,   headerHeight = parseInt($header.height(), 10)
 		,   footerHeight = parseInt($footer.height(), 10)
 		,   mainHeight = priHeight - headerHeight - footerHeight
-		,   sectionMargin = parseInt($('main section').css('margin-bottom'), 10)
-		,   sectionScale = mainHeight / (parseInt($('main section').outerHeight(), 10) + sectionMargin)
+		,   mainWidth = parseInt($main.width(), 10)
+		,   sectionMargin = parseInt($('.slideContainer > section').css('margin-bottom'), 10)
+		,   sectionMarginLeft = parseInt($('.slideContainer > section').css('margin-left'), 10)
+		,   sectionScale = (mainHeight < mainWidth) ?
+				mainHeight / (parseInt($('.slideContainer > section').outerHeight(), 10) + sectionMargin) :
+				mainWidth / (parseInt($('.slideContainer > section').outerWidth(), 10) + sectionMarginLeft*2)
 		;
 
 		console.log(sectionScale);
@@ -36,6 +46,7 @@ define([
 		$main.outerHeight(priHeight - headerHeight - footerHeight);
 
 		$('.slideContainer').css({
+			'transform': 'scale(' + sectionScale + ')',
 			'-webkit-transform': 'scale(' + sectionScale + ')',
 			'-ms-transform': 'scale(' + sectionScale + ')',
 			'-moz-transform': 'scale(' + sectionScale + ')'
@@ -70,7 +81,86 @@ define([
 		});
 	};
 
+	ret.setSlide = function (slide, navigate) {
+		if (!parseInt(slide, 10)) {
+			console.log(slide + " is not a valid slide number");
+			ret.slide = 1;
+		} else {
+			ret.slide = slide;
+			if (ret.slide < 0) {
+				ret.slide = 1;
+			}
+			if (ret.slide > $sections.length) {
+				ret.slide = $sections.length;
+			}
+		}
 
+		var $slide = $($sections[ret.slide-1]);
+		window.location.hash = ret.slide;
+
+
+		//console.log('ret.slide: ' + ret.slide);
+		if (navigate) {
+			//console.log('navigate to: ' + $main.scrollTop() + $slide.offset().top + parseInt($slide.css('margin-top'), 10));
+			$main.animate({
+				'scrollTop':  $main.scrollTop() + $slide.offset().top - parseInt($slide.css('margin-top'), 10) * 1.5
+			}, 160, function () {
+				$('.slideIndicator').html("Slide " + ret.slide + " of " + $sections.length);
+				if (ret.slide !== ret.lastSlide) {
+					ret.goClippy();
+					ret.lastSlide = ret.slide;
+				}
+			});
+		} else {
+			$('.slideIndicator').html("Slide " + ret.slide + " of " + $sections.length);
+		}
+	};
+
+	ret.slideUp = function () {
+		ret.setSlide(ret.slide-1, true);
+	};
+
+	ret.slideDown = function () {
+		ret.setSlide(ret.slide+1, true);
+	};
+
+	ret.lockOn = function () {
+		ret.setSlide(ret.slide, true);
+	};
+
+	ret.goTop = function () {
+		ret.setSlide(1, true);
+	};
+
+	ret.goBottom = function () {
+		ret.setSlide($sections.length, true);
+	};
+
+	ret.goClippy = function () {
+		var text = $($sections[ret.slide-1]).find('notes').text();
+		console.log(text);
+
+		if (!agent) {
+			return;
+		}
+		else {
+			agent.stopCurrent();
+			if (text && !text.match(/^\s*$/)) {
+				agent.speak(text);
+			} else {
+				agent.animate();
+			}
+		}
+	};
+
+	clippy.load('Clippy', function (clipmeister) {
+		agent = clipmeister;
+		agent.show();
+		setTimeout(function () {
+			$('.clippy, .clippy-balloon').appendTo('main');
+			ret.resize();
+		}, 300);
+	});
 	window.loadFrameSrc = ret.loadFrameSrc;
 
 	return ret;
